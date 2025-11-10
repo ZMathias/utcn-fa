ed9ccef52aeecec395b49e386f7e4c9784b9c0c4
Mathias <matyas.zagoni@proton.me>
2025-11-10 21:09:53 +0200

    Implemented hash, search, insert, delete

--- lab05/hash_table.cpp
+++ lab05/hash_table.cpp
@@ -4,6 +4,97 @@
 
 namespace lab05
 {
+    HashMapT* create_hashmap() {
+        auto* h_map = new HashMapT;
+        h_map->arr = new Entry*[PRIME_DIVIDER]; // memory management is deferred to the user
+        h_map->size = PRIME_DIVIDER;
+        memset(h_map->arr, 0, PRIME_DIVIDER); // zero memory
+
+        return h_map;
+    }
+
+    void delete_hashmap(HashMapT* h_map) {
+        for (int i = 0; i < h_map->size; i++) {
+            delete h_map->arr[i]; // delete each entry
+        }
+
+        delete[] h_map->arr;
+        delete h_map; // delete the array itself
+    }
+
+    int hash(const int key, const int idx) {
+        constexpr int c1 = 1;
+        constexpr int c2 = 1;
+        return (key + c1*idx + c2*idx*idx) % PRIME_DIVIDER;
+    }
+
+    Entry** search(HashMapT* h_map, const int key) {
+        Entry** arr = h_map->arr;
+
+        for (int i = 0; i < h_map->size; i++) {
+            const int h = hash(key, i);
+            if (arr[h] == nullptr) {
+                return nullptr; // empty slot found, no need to continue
+            }
+
+            if (arr[h] == TOMBSTONE) {
+                continue; // can not reference deleted slots, must continue
+            }
+
+            if (arr[h]->id == key) {
+                return arr + h; // key found, return pointer
+            }
+        }
+
+        return nullptr; // none found, nullptr signifies miss
+    }
+
+    bool insert(HashMapT* h_map, Entry* value) {
+        if (value == nullptr) {
+            return false;
+        }
+
+        // we must first find the empty slot into which we insert
+        Entry** arr = h_map->arr;
+        int first_tombstone = -1;
+        for (int i = 0; i < h_map->size; i++) {
+            const int h = hash(value->id, i);
+
+            if (arr[h] == nullptr) {
+                // free slot found, check if no tombstone was found previously and insert
+                if (first_tombstone != -1) {
+                    arr[first_tombstone] = value;
+                    return true;
+                }
+
+                arr[h] = value;
+                return true;
+            }
+
+            if (arr[h] == TOMBSTONE) {
+                if (first_tombstone == -1) {
+                    first_tombstone = h;
+                }
+            }
+        }
+
+        return false; // no slot found
+    }
+
+    bool delete_entry(HashMapT* h_map, const int key) {
+        Entry** index = search(h_map, key);
+        if (index == nullptr) {
+            return false; // not in table
+        }
+
+        delete *index; // delete structure pointed to by index
+        *index = TOMBSTONE;
+
+        return true;
+    }
+
+
+
     void demonstrate(int size) {
         printf("demonstrate!\n");
     }
--- lab05/hash_table.h
+++ lab05/hash_table.h
@@ -6,6 +6,40 @@
 
 namespace lab05
 {
+    // required default data structure from lab-assignment
+    struct Entry {
+        int id;
+        char name[30];
+    };
+
+    constexpr int PRIME_DIVIDER = 10007;
+    #define TOMBSTONE (reinterpret_cast<Entry*>(0xDEADBEEF))
+
+    struct HashMapT
+    {
+        Entry** arr;
+        int size;
+    };
+
+    /**
+     *
+     * @return Returns a pointer to the data structure of type HashMapT
+     */
+    HashMapT* create_hashmap();
+    void delete_hashmap(HashMapT* h_map, int size);
+
+    /**
+     * The hash function implements modulo hashing using a prime number.
+     * @param key The key to hash.
+     * @param idx The search iteration the hash is called in.
+     * @return hashed index
+     */
+    int hash(int key, int idx = 0);
+
+    Entry** search(HashMapT* h_map, int key);
+
+    bool insert(HashMapT* h_map, Entry* value);
+
     /**
      * @brief Demo code for the sorting algorithms
      *
