d4cfe1a650549420d6b9953cb960621fc905ee97
Mathias <matyas.zagoni@proton.me>
2025-12-15 18:00:02 +0200

    Made improvements to boilerplate

--- lab10/dfs.cpp
+++ lab10/dfs.cpp
@@ -3,6 +3,29 @@
 
 namespace lab10
 {
+    void dfs_rec(Graph& g, int from) {
+        for (auto n_idx : g[from].adj) {
+            Node* neighb = &g[n_idx];
+            if (neighb->color == COLOR_WHITE) {
+                neighb->color = COLOR_GRAY;
+                neighb->parent = from; // set current node as parent
+                const int idx = neighb - g.data(); // subtract the address of the node from the vector base address gives element index in array
+                dfs_rec(g, idx);
+            }
+        }
+
+        g[from].color = COLOR_BLACK;
+    }
+
+    void dfs(Graph& g) {
+        for (int i = 0; i < g.size(); i++) {
+            if (g[i].color == COLOR_WHITE) {
+                g[i].color = COLOR_GRAY;
+                dfs_rec(g, i);
+            }
+        }
+    }
+
     void pretty_print(const std::vector<int>& parent, const int node, const int depth) {
         constexpr int spaces_per_depth = 2;
 
@@ -17,14 +40,31 @@ namespace lab10
         }
     }
 
+    void print_graph(const Graph& g) {
+        for (int i = 0; i < g.size(); i++) {
+            std::print("{}: [", i);
+            for (const auto to : g[i].adj) {
+                if (to) {
+                    std::print(" {} ", to);
+                }
+            }
+            std::println("]");
+        }
+    }
+
     void demonstrate() {
         constexpr int numNodes = 6;
-        std::vector<Node> adj(numNodes, {COLOR_WHITE, -1, std::list<Node*>{}});
-        adj[0].adj = {&adj[1], &adj[2]};
-        adj[1].adj = {&adj[2], &adj[3]};
-        adj[2].adj = {&adj[3]};
-        adj[3].adj = {&adj[4]};
-        adj[4].adj = {&adj[5]};
+        std::vector<Node> g(numNodes, {COLOR_WHITE, -1, std::list<int>{}});
+        g[0].adj = {1, 2};
+        g[1].adj = {2, 3};
+        g[2].adj = {3};
+        g[3].adj = {4};
+        g[4].adj = {5};
+
+        print_graph(g);
+
+        // TODO: call dfs
+        dfs(g);
     }
 
     void performance(Profiler profiler) {
--- lab10/dfs.h
+++ lab10/dfs.h
@@ -7,18 +7,16 @@
 namespace lab10
 {
     enum {
-        COLOR_WHITE, COLOR_GRAY, COLOR_BLACK;
+        COLOR_WHITE, COLOR_GRAY, COLOR_BLACK
     };
 
     struct Node {
         char color;
         int parent;
-        std::list<Node*> adj;
+        std::list<int> adj;
     };
 
-    std::list<std::vector<int>> dfs(std::vector<std::list<int>>& adj);
-    void dfs_rec(std::vector<std::list<int>>& adj, std::vector<bool>& visited, std::vector<int>& par_arr, int source, int parent);
-    void pretty_print(const std::vector<int>& parent, int node = -1, int depth = 0);
+    using Graph = std::vector<Node>;
 
     void performance(Profiler profiler);
     void demonstrate();
